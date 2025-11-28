942988152d9514a833b453ef601cfe7b55a41b73
mascheyola <mascheyola@gmail.com>
2025-11-28 17:08:24 +0000

    fix: quitar botón de navegación hacia atrás de la pantalla principal.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,6 +12,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: Row(
           children: [
             const Icon(Icons.recycling),
